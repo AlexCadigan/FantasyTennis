08ec266e0a3feac0f39e31dcac17c3d3ef1434ab
Alex Cadigan <cadiganalexander@gmail.com>
2020-06-02 22:58:46 -0500

    Saving work.

--- Src/CronJobScripts/OnceADay/GetWeeksAndTournaments.php
+++ Src/CronJobScripts/OnceADay/GetWeeksAndTournaments.php
@@ -9,7 +9,10 @@ require_once("../Lib/HelpfulFunctions.php");
 
 $currentYear = date("Y");
 setupThisYearData($connection, $currentYear);
-removeOldData($connection, $currentYear);
+
+// Remove data that is more than 2 years old
+mysqli_query($connection, "DELETE FROM Weeks WHERE YEAR(StartDate)<" . ($currentYear - 1));
+mysqli_query($connection, "DELETE FROM Tournaments WHERE Year<" . ($currentYear - 1));
 
 /**
  * Sets up for scraping and analyzing the week and tournament data for this year.
@@ -23,7 +26,7 @@ function setupThisYearData($connection, $year) {
 	$currentWeek = $databaseCurrentWeek = mysqli_fetch_assoc(mysqli_query($connection, "SELECT Week FROM Weeks WHERE IsCurrent=1 AND YEAR(StartDate)=" . $year))["Week"];
 
 	// If there's no data yet for this year
-	if ($currentWeek == null) {
+	if (!$currentWeek) {
 		$currentWeek = 1;
 		$startDate = null;
 		getData($connection, $year, $currentWeek, $startDate);
@@ -34,9 +37,15 @@ function setupThisYearData($connection, $year) {
 		$startDate = strtotime(mysqli_fetch_assoc(mysqli_query($connection, "SELECT StartDate FROM Weeks WHERE Week=" . $currentWeek . " AND YEAR(StartDate)=" . $year))["StartDate"]);
 
 		// If start date is null it means there are no tournaments 2 weeks in the future
-		if ($startDate != null) {
+
+
+		// Not true, it could mean there's no data for the future because the tournaments weren't added yet.  They could be added later.
+
+		// Calculate the previous start date.  If that's null, calculate the current week's start date.  This is in case there isn't data yet for future weeks.
+
+		//if ($startDate != null) {
 			getData($connection, $year, $currentWeek, $startDate);
-		}
+		//}
 	}
 
 	setupNextYearData($connection, $year + 1, $databaseCurrentWeek);
@@ -55,7 +64,7 @@ function setupNextYearData($connection, $nextYear, $currentYearCurrentWeek) {
 	$currentYearNextWeek = mysqli_fetch_assoc(mysqli_query($connection, "SELECT Week FROM Weeks WHERE Week=" . ($currentYearCurrentWeek + 1) . " AND YEAR(StartDate)=" . ($nextYear - 1)))["Week"];
 
 	// If we are on the last week of this year (there is no next week for this year)
-	if ($currentYearNextWeek == null) {
+	if (!$currentYearNextWeek) {
 		// Start looking at next year's second week (so we don't mess up the first week's data)
 		$nextYearCurrentWeek = 2;
 	}
@@ -66,7 +75,7 @@ function setupNextYearData($connection, $nextYear, $currentYearCurrentWeek) {
 	$nextYearStartDate = strtotime(mysqli_fetch_assoc(mysqli_query($connection, "SELECT StartDate FROM Weeks WHERE Week=" . $nextYearCurrentWeek . " AND YEAR(StartDate)=" . $nextYear))["StartDate"]);
 
 	// If start date is null it means we don't have any data yet for the next year
-	if ($nextYearStartDate == null) {
+	if (!$nextYearStartDate) {
 		$nextYearCurrentWeek = 1;
 	}
 
@@ -76,8 +85,8 @@ function setupNextYearData($connection, $nextYear, $currentYearCurrentWeek) {
 /**
  * Scrapes tournament data from the ATP website and analyzes and stores the week and tournament data in the database.
  *
- * @param $connection - The database connection.
- * @param $year       - The year to use when getting/setting data.
+ * @param $connection  - The database connection.
+ * @param $year        - The year to use when getting/setting data.
  * @param $currentWeek - The current week.
  * @param $startDate   - The start date of the first week to look at.
  *
@@ -92,7 +101,6 @@ function getData($connection, $year, $currentWeek, $startDate) {
 
 	// Tournament and tournament dates counts should match
 	if ($numTournaments !== count($dates)) {
-		echo "Tournament and date counts do not match.";
 		return;
 	}
 
@@ -113,28 +121,26 @@ function getData($connection, $year, $currentWeek, $startDate) {
 		}
 
 		// If there is no data for this year yet and this is the first tournament encountered
-		if ($startDate == null && $previousDate == null) {
+		if (!$startDate && !$previousDate) {
 			// Create the first week and set it to the current week
 			mysqli_query($connection, "INSERT INTO Weeks (Week, StartDate, IsCurrent) VALUES (" . $currentWeek . ", '" . $dates[$i] . "', 1)");
 		}
 
 		// There is not data for this year yet or this tournament is 2 weeks in the future
-		if ($startDate == null || $unixDate >= $startDate) {
+		if (!$startDate || $unixDate >= $startDate) {
 			// This is so that the first week gets verified
-			if ($firstWeek && $startDate != null) {
+			if ($firstWeek && $startDate) {
+				verifyOrAddWeek($connection, $currentWeek, $dates[$i], $year);
+			}
+			// This tournament signifies the start of a new week
+			else if ($previousDate && (($unixDate - $previousDate) / 60 / 60 / 24) > 3) {
+				$currentWeek ++;
 				verifyOrAddWeek($connection, $currentWeek, $dates[$i], $year);
 			}
 			$firstWeek = false;
 
-			// This tournament signifies the start of a new week
-			if ($previousDate != null && (($unixDate - $previousDate) / 60 / 60 / 24) > 3) {
-				$currentWeek ++;
-				verifyOrAddWeek($connection, $currentWeek, $dates[$i], $year);
-			}
-
 			// Verify or add this tournament to the database
-			$row = mysqli_fetch_assoc(mysqli_query($connection, "SELECT Week FROM Tournaments WHERE Name='" . $tournaments[$i] . "' AND Year=" . $year));
-			if ($row == null) {
+			if (!$row = mysqli_fetch_assoc(mysqli_query($connection, "SELECT Week FROM Tournaments WHERE Name='" . $tournaments[$i] . "' AND Year=" . $year));) {
 				mysqli_query($connection, "INSERT INTO Tournaments (Name, Week, Year) VALUES ('" . $tournaments[$i] . "', " . $currentWeek . ", " . $year . ")");
 			}
 			else {
@@ -148,13 +154,13 @@ function getData($connection, $year, $currentWeek, $startDate) {
 	}
 
 	// Don't need to delete invalid data since the it was just filled for the first time
-	if ($startDate == null) {
+	if (!$startDate) {
 		return;
 	}
 
 	//Remove weeks that no longer exist
 	$databaseWeeks = mysqli_query($connection, "SELECT Week FROM Weeks WHERE YEAR(StartDate)=" . $year);
-	while ($row = mysqli_fetch_assoc($databaseWeeks) != null) {
+	while ($row = mysqli_fetch_assoc($databaseWeeks)) {
 		if ($row["Week"] > $currentWeek) {
 			mysqli_query($connection, "DELETE FROM Weeks WHERE Week=" . $row["Week"] . " AND YEAR(StartDate)=" . $year);
 		}
@@ -162,7 +168,7 @@ function getData($connection, $year, $currentWeek, $startDate) {
 
 	//Remove tournaments that no longer exist
 	$databaseTournaments = mysqli_query($connection, "SELECT Name, Week FROM Tournaments WHERE Year=" . $year);
-	while ($row = mysqli_fetch_assoc($databaseTournaments) != null) {
+	while ($row = mysqli_fetch_assoc($databaseTournaments)) {
 		if (!in_array($row["Name"], $tournaments) && $row["Week"] >= $databaseCurrentWeek) {
 			mysqli_query($connection, "DELETE FROM Tournaments WHERE Name=" . $row["Name"] . " AND Year=" . $year);
 		}
@@ -182,9 +188,9 @@ function getData($connection, $year, $currentWeek, $startDate) {
  */
 function verifyOrAddWeek($connection, $week, $startDate, $year) {
 	$row = mysqli_fetch_assoc(mysqli_query($connection, "SELECT StartDate FROM Weeks WHERE Week=" . $week . " AND YEAR(StartDate)=" . $year));
-	
+
 	// This week is not in the database
-	if ($row == null) {
+	if (!$row) {
 		mysqli_query($connection, "INSERT INTO Weeks (Week, StartDate, IsCurrent) VALUES (" . $week . ", '" . $startDate . "', 0)");
 	}
 	else {
@@ -193,9 +199,5 @@ function verifyOrAddWeek($connection, $week, $startDate, $year) {
 			mysqli_query($connection, "UPDATE Weeks SET StartDate='" . $startDate . "' WHERE Week=" . $week . " AND YEAR(StartDate)=" . $year);
 		}
 	}
-}
-
-function removeOldData($connection, $year) {
-
 }
 ?>
