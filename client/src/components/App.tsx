b0da3e0c26e33a8219b78340e538c526ff89eb61
Alex Cadigan <cadiganalexander@gmail.com>
2023-03-10 23:40:22 -0600

    Set document title dynamically

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,18 @@
 import LoginPage from "./LoginPage/LoginPage";
 import React from "react";
+import { resx } from "../Resources/Resources";
 
 /**
  * Main component class representing the application.
  */
 export default class App extends React.Component {
+	/**
+	 * Called before this component is rendered.  Sets the document title.
+	 */
+	public override componentDidMount(): void {
+		document.title = resx.tabTitle;
+	}
+
 	/**
 	 * Generate HTML content for the component.
 	 * @returns {JSX.Element} JSX element representing the application.
