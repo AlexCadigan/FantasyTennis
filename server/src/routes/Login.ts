ab4cfd3bd95f0d7a4da5fa810e6484a778ef4893
Alex Cadigan <cadiganalexander@gmail.com>
2023-03-12 22:27:56 -0500

    Remove debugging

--- server/src/routes/Login.ts
+++ server/src/routes/Login.ts
@@ -3,11 +3,8 @@ import APIs from "./routes";
 
 const router = express.Router();
 
-console.log("made it here");
-
 router.get(APIs.signIn, (_req: Request, res: Response) => {
 	res.status(200).send("test");
-	console.log("Inside this request");
 });
 
 export default router;
